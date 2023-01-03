298ca44721356a72f46c6b3f7d33e796fc45e54b
TheVoidThatConsumes <lyricalluxor@gmail.com>
2023-01-03 11:07:23 +0100

    Initial Commit
    Added custom colors, fonts and image resources to the assets folder.

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'main': '#050313',  
+        'primary': '#ff1f00',
+        'secondary': '#ff6b00',
+        'tertiary':'#ffc700',     
+      },
+      fontFamily: {
+        'heading': ['Sujoka'],
+        'logo': ['Noto-Sans-Javanese'],
+        'body': ['Montserrat'],
+      },
+    },
+  },
+  plugins: [],
+}
