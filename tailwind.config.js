db4e457900cb0ede6df568ee04e27ff1771fd9d6
TheVoidThatConsumes <lyricalluxor@gmail.com>
2023-01-06 11:11:26 +0100

    2nd Commit
    Added a few new pictures to replace the stock photos. Built the About section.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,11 @@ module.exports = {
         'main': '#050313',  
         'primary': '#ff1f00',
         'secondary': '#ff6b00',
-        'tertiary':'#ffc700',     
+        'tertiary':'#ffc700',
       },
+
       fontFamily: {
-        'heading': ['Sujoka'],
+        'heading': ['Sujoka', 'sans-serif'],
         'logo': ['Noto-Sans-Javanese'],
         'body': ['Montserrat'],
       },
